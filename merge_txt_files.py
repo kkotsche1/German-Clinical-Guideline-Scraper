35ab99ef2804298df929705343c6582e658c23bc
Konstantin Kotschenreuther <kkotsche1@gmail.com>
2023-04-18 00:25:22 +0200

    Improved datamining pipeline

--- merge_txt_files.py
+++ merge_txt_files.py
@@ -1,10 +1,11 @@
 import os
+import re
 
 # Set the path of the directory containing the text files
 dir_path = "./txt_files"
 
 # Set the name of the output file
-output_file = "merged_clinical_guidelines_short" \
+output_file = "merged_clinical_guidelines_v2" \
               ".txt"
 
 # Open the output file in append mode
@@ -13,11 +14,9 @@ with open(output_file, "a", encoding='utf-8') as f_out:
     # Iterate over all files in the directory
     for file_name in os.listdir(dir_path):
 
-        # Check if the file is a text file
-        if "Diabetes" in file_name:
-            print(file_name)
             # Open the file in read mode
             with open(os.path.join(dir_path, file_name), encoding='utf-8') as f_in:
 
                 # Read the contents of the file and write them to the output file
-                f_out.write(f_in.read())
+                #Using re.sub() to remove in text reference numbers
+                f_out.write(re.sub(r'\[\d+\]', '', f_in.read()))
--- pdf2text.py
+++ pdf2text.py
@@ -5,66 +5,72 @@ import re
 
 def pdf_to_txt(pdf_path, txt_path):
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        text = ''
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            cropbox = page.cropbox
-            cropbox.lower_left = (cropbox.lower_left[0], cropbox.lower_left[1] + 100)
-            cropbox.upper_right = (cropbox.upper_right[0], cropbox.upper_right[1] - 100)
-            page.cropbox = cropbox
-            text += page.extract_text()
 
         try:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            text = ''
+            for page_num in range(len(pdf_reader.pages)):
+                    page = pdf_reader.pages[page_num]
+                    cropbox = page.cropbox
+                    cropbox.lower_left = (cropbox.lower_left[0], cropbox.lower_left[1] + 100)
+                    cropbox.upper_right = (cropbox.upper_right[0], cropbox.upper_right[1] - 100)
+                    page.cropbox = cropbox
+                    text += page.extract_text()
+                    text1 = text
+
             if text.count("Abbildungsverzeichnis") >= 2:
-                text1 = re.split("Abbildungsverzeichnis", text, flags=re.IGNORECASE)
-                text1 = text1[1:len(text1) - 1]
-                text1 = " ".join(text1)
+                    text1 = re.split("Abbildungsverzeichnis", text, flags=re.IGNORECASE)
+                    text1 = text1[1:len(text1) - 1]
+                    text1 = " ".join(text1)
 
-            if text.count("Algorithmen") >= 2:
-                text1 = re.split("Algorithmen", text, flags=re.IGNORECASE)
-                text1 = text1[1:len(text1) - 1]
-                text1 = " ".join(text1)
+            elif text.count("Algorithmen") >= 2:
+                    text1 = re.split("Algorithmen", text, flags=re.IGNORECASE)
+                    text1 = text1[1:len(text1) - 1]
+                    text1 = " ".join(text1)
 
-            if text.count("Anhang") >= 2:
-                text1 = re.split("Anhang", text, flags=re.IGNORECASE)
-                text1 = text1[1:len(text1) - 1]
-                text1 = " ".join(text1)
+            elif text.count("Anhang") >= 2:
+                    text1 = re.split("Anhang", text, flags=re.IGNORECASE)
+                    text1 = text1[1:len(text1) - 1]
+                    text1 = " ".join(text1)
 
             elif text.count("Literaturverzeichnis") >= 2:
-                text1 = re.split("Literaturverzeichnis", text, flags=re.IGNORECASE)
-                text1 = text1[1:len(text1) - 1]
-                text1 = " ".join(text1)
+                    text1 = re.split("Literaturverzeichnis", text, flags=re.IGNORECASE)
+                    text1 = text1[1:len(text1) - 1]
+                    text1 = " ".join(text1)
 
             elif text.count("Literatur") >= 2:
-                text1 = re.split("Literatur", text, flags=re.IGNORECASE)
-                text1 = text1[1:len(text1)-1]
-                text1 = " ".join(text1)
+                    text1 = re.split("Literatur", text, flags=re.IGNORECASE)
+                    text1 = text1[1:len(text1)-1]
+                    text1 = " ".join(text1)
+            try:
+                if len(text1.split("Literatur")) > 1:
+                    text1 = " ".join(text1.split("Literatur")[:len(text1.split("Literatur"))-1])
+            except:
+                print("##############################################################################")
 
-            elif "Einleitung" in text:
-                text1 = re.split("Einleitung", text, flags=re.IGNORECASE)[1]
-                text1 = re.split("Literaturnachweis", text, flags=re.IGNORECASE)[0]
+            try:
+                if len(text1.split("Zielsetzung")) > 1:
+                    text1 = " ".join(text1.split("Zielsetzung")[1:])
+            except:
+                print("##############################################################################")
 
-            elif "Einführung" in text:
-                text1 = re.split("Einleitung", text, flags=re.IGNORECASE)[1]
-                text1 = re.split("Literatur", text, flags=re.IGNORECASE)[0]
+            print(f"Original Length: {len(text)}, New Length: {len(text1)}      {pdf_path}")
 
-            else:
-                text1 = text
+            with open(txt_path, 'w', encoding='utf-8') as txt_file:
+                txt_file.write(text1)
+
         except:
-            print("EXCEPT   ", pdf_path)
+            print("DIDN'T WORK ", pdf_path)
 
-        with open(txt_path, 'w', encoding='utf-8') as txt_file:
-            txt_file.write(text1)
+
 
 # Example usage:
 pdf_folder = "C:/Users/Admin/PycharmProjects/Leitlinien_scraper/pdf_files"
 txt_folder = "C:/Users/Admin/PycharmProjects/Leitlinien_scraper/txt_files"
 if not os.path.exists(txt_folder):
     os.makedirs(txt_folder)
-for filename in tqdm(os.listdir(pdf_folder)):
+for filename in os.listdir(pdf_folder):
     if filename.endswith('.pdf'):
-        if "Diabetes" in filename:
             if "abgelaufen" not in filename and "ungueltig" not in filename:
                     pdf_path = os.path.join(pdf_folder, filename)
                     txt_path = txt_folder + "/" + filename.replace(".pdf", ".txt")
