d9714e3602509ed9783544120be561e9fd7ded01
Konstantin Kotschenreuther <kkotsche1@gmail.com>
2023-04-20 09:48:40 +0200

    Data cleansing improvements

--- merge_txt_files.py
+++ merge_txt_files.py
@@ -16,7 +16,8 @@ with open(output_file, "a", encoding='utf-8') as f_out:
 
             # Open the file in read mode
             with open(os.path.join(dir_path, file_name), encoding='utf-8') as f_in:
+                text = re.sub(r'\[\d+\]', '', f_in.read())
 
                 # Read the contents of the file and write them to the output file
                 #Using re.sub() to remove in text reference numbers
-                f_out.write(re.sub(r'\[\d+\]', '', f_in.read()))
+                f_out.write(text)
